4169c47e3cf70df3f97a9c844d48043a30782bc5
Admin <Admin@DOTNET>
2019-07-24 00:05:08 -0700

    Add project files.

---
+++ src/Services/Transaction/Mappers/ModelMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Transaction.Framework.Domain;
+using Transaction.WebApi.Models;
+
+namespace Transaction.WebApi.Mappers
+{
+    public class ModelMappingProfile : Profile
+    {
+        public ModelMappingProfile()
+        {
+            CreateMap<TransactionModel, AccountTransaction>()
+                 .AfterMap<SetIdentityAction>()
+                 .ForAllMembers(opts => opts.Ignore());
+               
+            CreateMap<TransactionResult, TransactionResultModel>()
+                .ForMember(dest => dest.Balance, opt => opt.MapFrom(o => o.Balance.Amount.ToString("N")))
+                .ForMember(dest => dest.Currency, opt => opt.MapFrom(o => o.Balance.Currency.ToString())); 
+        }
+    }
+}
